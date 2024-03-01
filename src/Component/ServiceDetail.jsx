73048ed7db2586f9566b08d9f501edc88feb61cd
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-03-01 21:11:54 +0100

    done

--- src/Component/ServiceDetail.jsx
+++ src/Component/ServiceDetail.jsx
@@ -31,7 +31,7 @@ const ServiceDetail = () => {
         </div>
         <div className=" items-center justify-center p-4 bg-lightC sm:px-2 md:px-4 lg:px-6 lg:grid lg:gap-4 xl:px-32">
           <div className="lg:grid lg:grid-cols-2 items-center">
-            <div className="l p-4 mb-2  mx-4 bg-blueC rounded-md box-shad iix:px-2 iix:mx-0 ixr:px-2 ixi:px-2 ixr:mx-0 ixi:mx-0">
+            <div className=" min-[500px]:px-4 py-4 mb-2  min-[500px]:mx-4 bg-blueC rounded-md box-shad iix:px-2 iix:mx-0 ixr:px-2 ixi:px-2 ixr:mx-0 ixi:mx-0">
               <p className="text-white">{service.description}</p>
             </div>
 
@@ -44,7 +44,7 @@ const ServiceDetail = () => {
             </div>
           </div>
           <div className="lg:flex lg:flex-row-reverse items-center">
-            <div className="flex w-full lg:flex-col md:px-4 iix:flex-col ixr:grid ixi:grid iix:p-0 ">
+            <div className="min-[500px]:flex  w-full lg:flex-col md:px-4 iix:grid ixr:grid ixi:grid iix:p-0 ">
               <div className=" iix:min-w-0 p-4 mb-8 mx-2 bg-blueC rounded-md box-shad">
                 <h2 className="text-white text-2xl mb-4 font-bold">
                   {service.h_one}
