71e38f325b554750c5e5389e5d369eb5e909e1ff
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-03-01 23:28:12 +0100

    hh

--- src/Component/GalleryImg.jsx
+++ src/Component/GalleryImg.jsx
@@ -1,69 +1,55 @@
 import React, { useRef } from "react";
-import aft1 from "../Images/aft1.jpg";
+import "../hero.css";
 import bef1 from "../Images/bef1.jpg";
-import aft2 from "../Images/aft2.jpg";
+import aft1 from "../Images/aft1.jpg";
 import bef2 from "../Images/bef2.jpg";
+import aft2 from "../Images/aft2.jpg";
+import brick from "../Images/icons/light.png";
+import ReactCompareImage from "react-compare-image";
+import useZoomInAnimation from "../animation/useZoomInAnimation";
 
-const ImageComparison = () => {
-  const resizerRef = useRef(null);
-
-  const handleTouchStart = (e) => {
-    e.preventDefault();
-    document.addEventListener("touchmove", handleTouchMove, false);
-    document.addEventListener("touchend", handleTouchEnd, false);
-  };
-
-  const handleTouchMove = (e) => {
-    const resizer = resizerRef.current;
-    const deltaX = e.touches[0].clientX - resizer.getBoundingClientRect().left;
-    resizer.style.left = `${deltaX}px`;
-  };
-
-  const handleTouchEnd = () => {
-    document.removeEventListener("touchmove", handleTouchMove);
-    document.removeEventListener("touchend", handleTouchEnd);
-  };
+const GalleryImg = () => {
+  const containerRef = useRef(null);
+  useZoomInAnimation(".zoom");
 
   return (
-    <div className="lg:grid lg:grid-cols-2 sticky gap-4 items-center">
-      <div className="mb-4">
-        <div className="diff aspect-[15/12] relative">
-          <div className="diff-item-1">
-            <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">
-              <img src={aft1} alt="After 1" />
-            </div>
+    <>
+      <div className="bg-cover bg-center justify-center text-center">
+        <div className="flex flex-col items-center relative my-8 zoom">
+          <img
+            src={brick}
+            className="max-w-full h-[3rem]  rounded-full bg-lightC p-2 absolute mt-[-2rem] box-shad "
+            alt="Flooring"
+          />
+          <h2 className="text-redC tsgB subhead xl:text-xl font-bold">
+            TRANSFORMATION
+          </h2>
+          <p className="text-blueC font-tsg mt-[3rem] text-xl bg-lightC p-2 absolute xl:text-[25px] rounded-xl">
+            Before & After
+          </p>
+        </div>
+
+        <div className="lg:grid lg:grid-cols-2  gap-4 items-center px-8">
+          <div className="mb-4 " ref={containerRef}>
+            <ReactCompareImage
+              leftImage={bef1}
+              rightImage={aft1}
+              aspectRatio="wider"
+              containerRef={containerRef}
+              className="box-shad  sticky gb"
+            />
           </div>
-          <div className="diff-item-2">
-            <div className="bg-base-200 text-9xl font-black grid place-content-center">
-              <img src={bef1} alt="Before 1" />
-            </div>
-          </div>
-          <div
-            className="diff-resizer"
-            onTouchStart={handleTouchStart}
-            ref={resizerRef}
-          ></div>
+          <ReactCompareImage
+            leftImage={bef2}
+            rightImage={aft2}
+            aspectRatio="wider"
+            containerRef={containerRef}
+            className="box-shad  sticky gb"
+          />
         </div>
       </div>
-      <div className="diff aspect-[15/11] relative">
-        <div className="diff-item-1">
-          <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">
-            <img src={aft2} alt="After 2" />
-          </div>
-        </div>
-        <div className="diff-item-2">
-          <div className="bg-base-200 text-9xl font-black grid place-content-center">
-            <img src={bef2} alt="Before 2" />
-          </div>
-        </div>
-        <div
-          className="diff-resizer"
-          onTouchStart={handleTouchStart}
-          ref={resizerRef}
-        ></div>
-      </div>
-    </div>
+    </>
   );
 };
 
-export default ImageComparison;
+export default GalleryImg;
